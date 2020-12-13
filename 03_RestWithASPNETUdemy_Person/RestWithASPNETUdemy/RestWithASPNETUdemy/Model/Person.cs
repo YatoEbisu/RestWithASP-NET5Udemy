3fa16206c7196d62bb2dd1cc65e4aa49194d7f9e
only-BSF <BrenoSilvaFortunato@gmail.com>
2020-12-13 12:50:51 -0300

    Working with four verbs

---
+++ 03_RestWithASPNETUdemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        [Key]
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
